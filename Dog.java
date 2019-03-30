d58be2f9034d4a207233f170612afb8f409f353c
vedavyas Burli <vedavyas.pb@gmail.com>
2019-03-31 00:35:12 +0530

    added Dog java file

---
+++ Dog.java
@@ -0,0 +1,20 @@
+public class Dog{
+	
+
+public static void main (String []args){
+	
+	System.out.println("hey");
+	barking =new bark();
+
+}
+void barking(){
+	System.out.println("Bark");
+}
+
+void hungery(){
+	System.out.println("Eat");
+}
+void sleeping(){
+	System.out.println("Sleep");
+}
+}
